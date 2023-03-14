d848b5e9fec14cbea25f8ec40b2b8a363d2ee847
Charles <charlespun6@gmail.com>
2023-03-14 16:30:02 -0400

    minus too

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -3,11 +3,15 @@ export default function QueryProcessor(query: string): string {
   if (str[3] === "multiplied" ) {
     //return (str[5].substring(0,str[5].length-2)).toString();
     var ret = Number(str[2])*Number(str[5].substring(0,str[5].length-2));
-    return (ret.toString(10));
+    return (ret.toString());
+  }
+  if (str[3] === "minus") {
+    var ret = Number(str[2])+Number(str[4].substring(0,str[4].length-2));
+    return (ret.toString());
   }
   if (str[3] === "plus") {
-    var ret = Number(str[2])+Number(str[4]);
-    return (ret.toString(10));
+    var ret = Number(str[2])+Number(str[4].substring(0,str[4].length-2));
+    return (ret.toString());
   }
   if (query.toLowerCase().includes("what is your name?")) {
     return ("charlespun");
