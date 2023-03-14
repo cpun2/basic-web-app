3df1c953abd3f35ee0e1a08b9208b1a19aee7d8c
Charles <charlespun6@gmail.com>
2023-03-14 16:38:11 -0400

    fixed minus

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,12 +1,20 @@
 export default function QueryProcessor(query: string): string {
   var str = query.split(" ");
+  /* if (str[6] === "primes") {
+    var numbers = str.slice(7,str.length);
+    var out = []
+    for (let i = 0; i < numbers.length; i++) {
+      var cur = numbers[i].substring(0,numbers[i].length-2);
+
+    }
+  } */
   if (str[3] === "multiplied" ) {
     //return (str[5].substring(0,str[5].length-2)).toString();
     var ret = Number(str[2])*Number(str[5].substring(0,str[5].length-2));
     return (ret.toString());
   }
   if (str[3] === "minus") {
-    var ret = Number(str[2])+Number(str[4].substring(0,str[4].length-2));
+    var ret = Number(str[2])-Number(str[4].substring(0,str[4].length-2));
     return (ret.toString());
   }
   if (str[3] === "plus") {
